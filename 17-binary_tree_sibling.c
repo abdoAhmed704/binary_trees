60954bdc1b7e2d74f0982393912cd0470080d27d
= <abdoahmedsayed704@gmail.com>
2024-03-01 02:15:54 +0200

    Done...

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,16 +6,9 @@
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node->parent == NULL || node == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
-
-	if (node->parent->right != NULL && node->parent->left != NULL)
-	{
-		if (node->parent->left == node)
-			return (node->parent->right);
-
-		if (node->parent->right == node)
-			return (node->parent->left);
-	}
-	return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
